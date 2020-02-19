395329a6ef04de31e97d8c64b20116685af67e0f
Jamie Baird <james.baird@softwire.com>
2020-02-19 13:05:26 +0000

    Started working on javascript functionality.
    Removing 'Posts' header upon clicking

---
+++ MyFace/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+/*
+window.addEventListener("load", function() {
+    console.log("Hello TechSwitch!");
+    let elements = document.getElementsByTagName("p"), i, len;
+    
+    for (i = 0, len = elements.length; i < len; i++) {
+        elements[i].style.color = 'red';
+    }
+});*/
+
+window.onclick = function(){
+    let button = document.getElementById("#tester");
+    button.remove();
+};
+
+
+
